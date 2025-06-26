f3ea28c05bce6be25d6898abe3a8bdd9bbb55c7b
yuki-rose <yuki@YI-LT-01.localdomain>
2025-06-27 00:36:18 +0900

    ArticleModel修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,8 @@
 
 ActiveRecord::Schema[7.2].define(version: 2025_06_26_142821) do
   create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
